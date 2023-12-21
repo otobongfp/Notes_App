1411a5c2767e43551019837312b07c3a7fc79935
unknown <otobongfp@gmail.com>
2023-12-21 05:18:17 +0100

    Create and Read feaures updated

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::{self, Write};
 use chrono::prelude::*;
-use serde_json::to_string;
+//use serde_json::to_string;
 
 
 #[derive(Debug)]
@@ -11,7 +11,7 @@ use serde_json::to_string;
 
 struct Note{
     id: u16,
-    //date: String,
+    date: DateTime<Local>,
     title : String,
     author : String,
     body : String,
@@ -21,7 +21,8 @@ struct Note{
 //     Id(Note),
 // }
 
-
+//To hold the structs on notes being pushed in.
+static  mut ALL_NOTES : Vec<Note> = Vec::new();
 
 fn main() {
 //[L] List all notes
@@ -29,6 +30,8 @@ fn main() {
 //[D] Delete Note
 //[U] Update an old note
 //[R] Read a note via a TUI.
+
+    
 
     println!("+----------------------------+\n|  WELCOME TO THE NOTES APP  |\n+----------------------------+");
     
@@ -47,47 +50,51 @@ fn main() {
     \t[D] Delete Note
     ");
     
-    //Choose a Command based on the above options
-    let mut choice = String::new();
+    loop{
 
-    print!("type here ---> ");
-    //Allows Us to Receive inputs on the same line
-    let _ = io::stdout().flush();
-    io::stdin().read_line(&mut choice).expect("Something is Wrong");
+        //Choose a Command based on the above options
+        let mut choice = String::new();
 
-    let choice = choice.trim().parse().unwrap();
+        print!("type here ---> ");
+        //Allows Us to Receive inputs on the same line
+        let _ = io::stdout().flush();
+        io::stdin().read_line(&mut choice).expect("Something is Wrong");
 
+        let choice = choice.trim().parse().unwrap();
+        match choice {
+            1 => {
+                println!("Creating New Note...");
+                
+                // let from_ymd_opt = NaiveDate::from_ymd_opt;
+                // assert!(from_ymd_opt(2015, 3, 14).is_some());
+        
+                print!("TITLE:   ");
+                let mut title = String::new();
+                let _ = io::stdout().flush();
+                io::stdin().read_line(&mut title).expect("Check the title");
+        
+                print!("Make Note:   ");
+                let mut body = String::new();
+                let _ = io::stdout().flush();
+                io::stdin().read_line(&mut body).expect("Check the title");
 
-    match choice {
-        1 => {
-            println!("Creating New Note...");
-            
-            let from_ymd_opt = NaiveDate::from_ymd_opt;
-            assert!(from_ymd_opt(2015, 3, 14).is_some());
+                //I cloned the name as the author in the function. This way we avoid
+                //Any possible memory pointer issues
+                create_note(now, title, name.clone(), body);
+            },
+            2 => {
+                //Reading the note
+                read_note();
+            },
+            _ => {
+                println!("Something is wrong");
+            },
     
-            print!("TITLE:   ");
-            let mut t = String::new();
-            let _ = io::stdout().flush();
-            io::stdin().read_line(&mut t).expect("Check the title");
     
-            print!("Make Note:   ");
-            let mut b = String::new();
-            let _ = io::stdout().flush();
-            io::stdin().read_line(&mut b).expect("Check the title");
-            
-            create_note(t, name, b);
-        },
-        3 => {
-            println!("{}", Note.title());
-        },
-        _ => {
-            println!("Something is wrong");
-        },
-
-
-    };
+        };
+    }
     
-    //CREATE NOTE
+    //CREATE NOTE - working
     
     //LIST NOTES
 
@@ -101,18 +108,48 @@ fn main() {
     
 }
 
-//CREATE NOTE
-pub fn create_note(title: String, name : String, body: String) -> Note {
 
-    Note{
-        id: 1,
-        title : title,
-        author : name,
-        body : body,
+pub fn create_note(date: DateTime<Local>, title: String, author: String, body: String) {
+
+    //You can use this but since the function names is similar
+    //to the names of the field on the struct we use single names
+    // let new_note = Note{
+    //     title: title,
+    //     author: author,
+    //     body: body,
+    // };
+    
+    unsafe{
+        let this_id = ALL_NOTES.len() as u16 + 1;
+
+        let new_note = Note{
+            id:this_id,
+            date,
+            title,
+            author,
+            body,
+        };
+    
+        ALL_NOTES.push(new_note);
     }
+
 }
 
-fn read_note(new_note.[n]){
-    let read_note = &new_note;
-    println!("Title:{}\nAuthor:{}\n",);
+pub fn read_note(){
+    //I used unsafe here to easily access the values in All notes
+    //There are options to avoid using unsafe but for simplicity
+    //I made everything as direct as possible, lol.
+    
+    println!("\n\n_________________________________________________CURRENT NOTES_________________________________________________\n");
+    unsafe{
+        for i in &ALL_NOTES{
+            //println!("{:?}", i);
+            let new_id = &i.id;
+            let new_date = &i.date;
+            let new_title = &i.title;
+            let new_name = &i.author;
+            let new_body = &i.body;
+            println!("ID[{new_id}] \nTITLE: {new_title}\nauthor: {new_name} \ncreated on: {new_date} \n\n\tnote: {new_body}");
+        }
+    }
 }
